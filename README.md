2092b066e7fde81f25d2d961d1f4fa07430ac065
empoleonz0 <gway@ucsd.edu>
2023-08-09 15:55:37 -0700

    created basic website

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PokemonShowdown
+
+Trying to create an accurate Pokemon battle application
---
+++ src/Components/App.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+
+const App = ()=> {
+  return (
+    <div>
+      <h1>Pokemon Showdown</h1>
+    </div>
+  );
+};
+
+export default App;
